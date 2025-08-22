e3c9a99890fb60b518d1101c939ae536c440d9f7
StanQ2 <gabito.correos@gmail.com>
2025-08-22 15:06:23 -0500

    Fusión exitosa
    Se logró fusionar las 2 ramas, con los cambios anteriores

---
+++ Scripts/Camera/camera_2d.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+
+signal reached_target(x: float)
+
+@export var speed: float = 60.0
+var target_x: float = 0.0
+var moving: bool = false
+
+func _ready() -> void:
+	position_smoothing_enabled = true
+	position_smoothing_speed = 4.0
+	make_current()
+	add_to_group("main_camera")
+
+func _process(delta: float) -> void:
+	if not moving: return
+	global_position.x = move_toward(global_position.x, target_x, speed * delta)
+	if is_equal_approx(global_position.x, target_x):
+		moving = false
+		emit_signal("reached_target", target_x)
+
+func go_to_x(x: float) -> void:
+	target_x = x
+	moving = true
+	$"../AnimationPlayer".play("arrow_go")
