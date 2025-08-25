269973431ba249231d2c5da09fc250e839733046
StanQ2 <gabito.correos@gmail.com>
2025-08-24 21:29:28 -0500

    fin

--- Scripts/Intefaz/pausa.gd
+++ Scripts/Intefaz/pausa.gd
@@ -5,3 +5,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("start"):
 		get_tree().paused = not get_tree().paused
 		$TextureRect.visible = not $TextureRect.visible
+		$Salir.visible = not $Salir.visible
+		$Salir/Label.visible = not $Salir/Label.visible
+
+func _on_salir_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Interfaz/Menu.tscn")
+	
+	
