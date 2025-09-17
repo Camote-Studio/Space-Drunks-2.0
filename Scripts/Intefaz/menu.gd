b421491c00add80d37c71393bb7643f2b9f647e0
StanQ2 <gabito.correos@gmail.com>
2025-09-17 13:06:49 -0500

    Menu animación
    Se agregó animación al menú

--- Scripts/Intefaz/menu.gd
+++ Scripts/Intefaz/menu.gd
@@ -2,15 +2,22 @@ extends Control
 
 var tipo_boton = null
 
+
 func _ready() -> void:
 	var music = preload("res://Assets/music/624874__sonically_sound__retro-funk-20032022-1714.wav")
 	SoundEffectManager.music_player.stream = music
 	SoundEffectManager.music_player.play()
+	
+	$"Sombras_transición".show()
+	$"Sombras_transición/AnimationPlayer".play("Sombra_off")
+	
+	
+	#ANIMACIÓN CON SHADERS SOLO AFECTA A LOS HIJOS DEL NODO CONTROL (SPRITE2D_TITULO, BOTONES):
 	for button in get_tree().get_nodes_in_group("ui_botones"):
 		if button is TextureButton:
 			button.mouse_filter = Control.MOUSE_FILTER_IGNORE
 			button.focus_mode = Control.FOCUS_ALL  # Sigue respondiendo a teclado/mando
-	$Start.grab_focus()
+	#$Start.grab_focus()
 
 func _on_start_pressed() -> void:
 	tipo_boton = "start"
@@ -33,4 +40,7 @@ func _on_sombra_time_timeout() -> void:
 	
 	elif tipo_boton == 'options' :
 		get_tree().change_scene_to_file("res://Scenes/Interfaz/Opciones_Menu.tscn") 
+		
+
+
 		
