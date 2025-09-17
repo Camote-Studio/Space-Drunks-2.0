4c8c0da3ef0624fd58df41005aa2161c69f02ae7
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-09-17 00:51:02 -0500

    add cinamtica and bossfight animation with powers

--- Scripts/follow.gd
+++ Scripts/follow.gd
@@ -18,8 +18,8 @@ func _ready() -> void:
 	_cd.one_shot = true
 	add_child(_cd)
 
-func enter(msg := {}) -> void:
-	super.enter(msg)
+func enter() -> void:
+	super.enter()
 	if _anim == null:
 		_anim = actor.get_node_or_null("AnimatedSprite2D")
 	if _anim:
@@ -30,15 +30,18 @@ func physics_update(delta: float) -> void:
 	if p == null:
 		fsm.transition_to("Idle")
 		return
+	if body == null:
+		push_warning("Follow: body es null. Revisa jerarquía del FSM.")
+		fsm.transition_to("Idle")
+		return
 
-	var to := p.global_position - actor.global_position
+	var to := p.global_position - body.global_position
 	var dist := to.length()
-	var dir
+	var dir := Vector2.ZERO
 	if dist > 0.0:
 		dir = to / dist
-	else:
-		dir = Vector2.ZERO
-	# Control proporcional para mantener una distancia
+
+
 	var err := dist - desired_dist
 	var target_vel := Vector2.ZERO
 	if abs(err) > deadzone:
@@ -46,18 +49,13 @@ func physics_update(delta: float) -> void:
 		var radial_speed = clamp(err * kp, -speed, speed)
 		target_vel = dir * radial_speed
 
-	# **Mover al boss** (CharacterBody2D)
-	var body := actor as CharacterBody2D
-	if body:
-		body.velocity = body.velocity.move_toward(target_vel, accel * delta)
-		body.move_and_slide()
+	body.velocity = body.velocity.move_toward(target_vel, accel * delta)
+	body.move_and_slide()
 
-	# Voltear sprite
 	if _anim:
 		_anim.flip_h = dir.x < 0.0
 		_anim.flip_v = false
 
-	# ¿Dash?
 	if (_cd.time_left <= 0.0) and (dist >= dash_min_range) and (dist <= dash_max_range):
 		fsm.transition_to("Dash", {"dir": dir})
 		_cd.start(dash_cooldown)
