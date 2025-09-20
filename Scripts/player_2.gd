e78954deb125fda40ff2acc2bfcebf1da947a1d2
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-09-20 13:27:54 -0500

    changes of player 2 coins

--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -525,11 +525,19 @@ func _on_timer_timeout() -> void:
 	if estado_actual == Estado.ATURDIDO:
 		estado_actual = Estado.NORMAL
 
+# ====================== FUNCIÓN PARA COLECCIONAR MONEDAS =======================
 func collect_coin(amount: int = 1) -> void:
 	coins += amount
 	if coin_label:
 		coin_label.text = str(coins)
 	GameState.set_coins(player_id, coins)
+	if coins >= 20:
+		bar.value = clamp(bar.value + 20, bar.min_value, bar.max_value)
+		#_show_shop()
+		coins = 0
+		if coin_label:
+			coin_label.text = str(coins)
+			GameState.set_coins(player_id, coins)
 
 func _on_veneno_timer_timeout() -> void:
 	if estado_actual == Estado.VENENO:
